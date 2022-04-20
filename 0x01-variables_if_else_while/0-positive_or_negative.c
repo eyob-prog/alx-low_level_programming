d08f4f55850ee6f293979854fdd040c1853be85a
eyob-prog <eyobabatel024@gmail.com>
2022-04-20 01:43:06 -0700

    Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main Entry point
+* main -  Entry point
 *
 * Description: Checks if number is greater or less or equale to 0
 * Return: Always (Success/correct)
